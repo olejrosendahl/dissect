e6e22552791c75cb40817f26fd0116309730bd1c
Ole J. Rosendahl <ole.johnny.rosendahl@gmail.com>
2015-09-08 21:07:30 +0700

    Update gem skeleton

---
+++ spec/dissect_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+describe Dissect do
+  it 'has a version number' do
+    expect(Dissect::VERSION).not_to be nil
+  end
+
+  it 'does something useful' do
+    expect(false).to eq(true)
+  end
+end
